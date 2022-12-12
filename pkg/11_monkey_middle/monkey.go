4321a5b1565f7bc721788f5b78f52bf9725e515b
Henry Peteet <hvpeteet@gmail.com>
2022-12-11 22:39:01 -0500

    Revert "tried bigint"
    This reverts commit 85806a0d675e7279e0c4cf744e3842bb0355aa39.

--- pkg/11_monkey_middle/monkey.go
+++ pkg/11_monkey_middle/monkey.go
@@ -2,50 +2,46 @@ package monkey_middle
 
 import (
 	"fmt"
-	"math/big"
 	"strconv"
 	"strings"
 )
 
 type Monkey struct {
-	Items          []*big.Int
+	Items          []int
 	Operation      string
-	TestDivsibleBy *big.Int
+	TestDivsibleBy int
 	TrueTarget     int
 	FalseTarget    int
 }
 
-func (m *Monkey) ApplyOperation(old *big.Int) (*big.Int, error) {
+func (m *Monkey) ApplyOperation(old int) (int, error) {
 	pieces := strings.Split(strings.TrimSpace(m.Operation), " ")
 	if len(pieces) != 6 {
-		return nil, fmt.Errorf("%s did not have exactly 6 elements divided by spaces", m.Operation)
+		return 0, fmt.Errorf("%s did not have exactly 6 elements divided by spaces", m.Operation)
 	}
 
 	operator := pieces[4]
 
-	var operand1, operand2 *big.Int
+	var operand1, operand2 int
+	var err error
 	if pieces[3] == "old" {
 		operand1 = old
-	} else if op1_small, err := strconv.Atoi(pieces[3]); err != nil {
-		return nil, err
-	} else {
-		operand1 = big.NewInt(int64(op1_small))
+	} else if operand1, err = strconv.Atoi(pieces[3]); err != nil {
+		return 0, err
 	}
 
 	if pieces[5] == "old" {
 		operand2 = old
-	} else if op2_small, err := strconv.Atoi(pieces[5]); err != nil {
-		return nil, err
-	} else {
-		operand2 = big.NewInt(int64(op2_small))
+	} else if operand2, err = strconv.Atoi(pieces[5]); err != nil {
+		return 0, err
 	}
 
 	switch operator {
 	case "+":
-		return (&big.Int{}).Add(operand1, operand2), nil
+		return operand1 + operand2, nil
 	case "*":
-		return (&big.Int{}).Mul(operand1, operand2), nil
+		return operand1 * operand2, nil
 	default:
-		return nil, fmt.Errorf("%s is not a valid operation", m.Operation)
+		return 0, fmt.Errorf("%s is not a valid operation", m.Operation)
 	}
 }
